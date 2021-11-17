ff29b58dc6f0d23a363be0a757b45bef815f06da
lizhiqing <lizhiqing>
2021-11-17 21:04:56 +0800

    mongodb

--- pkg/mongodb/mongodb.go
+++ pkg/mongodb/mongodb.go
@@ -17,10 +17,16 @@ var (
 func InitMongodb() {
 	host := viper.GetString("mongodb.host")
 	port := viper.GetString("mongodb.port")
-
+	user := viper.GetString("mongodb.user")
+	password := viper.GetString("mongodb.password")
 	mongodbUrl := host + ":" + port
 	// 建立mongodb连接
-	clientOptions := options.Client().ApplyURI(mongodbUrl).SetConnectTimeout(5 * time.Second)
+	clientOptions := options.Client().SetAuth(options.Credential{
+		AuthMechanism: "",
+		AuthSource:    "",
+		Username:      user,
+		Password:      password,
+	}).ApplyURI(mongodbUrl).SetConnectTimeout(5 * time.Second)
 	Client, err = mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		logger.Logger("pkg.mongodb.client").Error(err)
